dcab4485bae64ee414e6837bf93b3c2c545f8cc9
joshcapz <joshuacapacite983@gmail.com>
2025-08-28 11:43:40 +0800

    Firebase Configuration

---
+++ firebaseConfig.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCFMTJiEeVR_FXSUb7TSmgjsihlp19tT3s",
+  authDomain: "jgc-goal-tracker.firebaseapp.com",
+  projectId: "jgc-goal-tracker",
+  storageBucket: "jgc-goal-tracker.firebasestorage.app",
+  messagingSenderId: "375969020015",
+  appId: "1:375969020015:web:a9ef43664061df8d16f59a"
+};
+
+
+const app = initializeApp(firebaseConfig);
